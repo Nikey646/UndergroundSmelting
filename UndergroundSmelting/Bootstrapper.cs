9fe762ab2fdb1852a4aec3e5457688cb92ee7f32
Nikey646 <nikey646@users.noreply.github.com>
2016-08-05 05:50:12 +1000

    Initial Commit

---
+++ UndergroundSmelting/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using UndergroundSmelting.Machines;
+
+namespace UndergroundSmelting
+{
+	public class Bootstrap : FortressCraftMod
+	{
+		
+		public override ModRegistrationData Register()
+		{
+			var mrd = new ModRegistrationData();
+			mrd.RegisterEntityHandler(eSegmentEntity.OreSmelter);
+			return mrd;
+		}
+
+		public override void CreateSegmentEntity(ModCreateSegmentEntityParameters param, ModCreateSegmentEntityResults results)
+		{
+			if (param.Type == eSegmentEntity.OreSmelter)
+				results.Entity = UndergroundSmelter.CreateSmelter(param);
+		}
+
+	}
+}
---
+++ UndergroundSmelting/Machines/UndergroundSmelter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Object = System.Object;
+
+namespace UndergroundSmelting.Machines
+{
+	public class UndergroundSmelter : OreSmelter
+	{
+		private UndergroundSmelter(Segment segment, Int64 x, Int64 y, Int64 z, UInt16 cube, Byte flags, UInt16 lValue)
+			: base(segment, x, y, z, cube, flags, lValue)
+		{ }
+
+		internal static UndergroundSmelter CreateSmelter(ModCreateSegmentEntityParameters param)
+		{
+			var smelter = new UndergroundSmelter(param.Segment, param.X, param.Y, param.Z, param.Cube, param.Flags, param.Value)
+			{
+				mbTooDeep = false,
+				mrSmeltTime = 99.9f
+			};
+
+			if (DifficultySettings.mbCasualResource)
+				smelter.mrSmeltTime = 10f;
+			if (DifficultySettings.mbRushMode)
+				smelter.mrSmeltTime = 3f;
+			if (smelter.mValue == 1)
+			{
+				smelter.mrSmeltTime /= 2f;
+				smelter.mrPowerRate *= 2f;
+				smelter.mnOrePerBar *= 4;
+			}
+
+			return smelter;
+		}  
+	}
+}
